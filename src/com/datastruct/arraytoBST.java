e3f9bedb4a6d3dcdd1186055c68cf376426acfd2
Arun Vasudevan <arun.psg.eee@gmail.com>
2017-01-05 13:59:35 -0600

    Optimizing

--- src/com/datastruct/arraytoBST.java
+++ src/com/datastruct/arraytoBST.java
@@ -2,14 +2,10 @@ package src.com.datastruct;
 
 public class arraytoBST {
 	public TreeNode converttoBST(int[] arr){
-		final int start=0;
-		final int end=arr.length;
-		
-		return buildBST(arr,start,end);
+		return buildBST(arr,0,arr.length);
 	}
 	
 	private TreeNode buildBST(int[] arr, int start, int end){	
-		System.out.println("Start:"+start+", End:"+end);
 		if (start==end)
 			return null;
 		final int rootIndex=(start+end)/2;
