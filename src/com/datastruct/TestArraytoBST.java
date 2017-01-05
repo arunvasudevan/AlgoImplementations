850a7bbfdff06254e1cb0e4a707f648139a2d165
Arun Vasudevan <arun.psg.eee@gmail.com>
2017-01-05 14:03:53 -0600

    Optimized after review

--- src/com/datastruct/TestArraytoBST.java
+++ src/com/datastruct/TestArraytoBST.java
@@ -1,21 +1,21 @@
-package src.com.datastruct;
+package com.datastruct;
 
 public class TestArraytoBST {
 
 	public static void main(String[] args) {
 		final arraytoBST a=new arraytoBST();
-		final int[] arr={0,1,2,3,4,5};
+		final int[] arr={1,6,10,19};
 		
 		final TreeNode n=a.converttoBST(arr);
-		
+		System.out.println("Sorted Array Inserted to a Tree....");
 		final TestArraytoBST t=new TestArraytoBST();
+		System.out.println("Printing the Tree:");
 		t.depthFirstSearch(n);
 	}
 
 	public void depthFirstSearch(TreeNode n){
 		if(n==null)
 			return;
-		
 		depthFirstSearch(n.left);
 		System.out.println(n.data);
 		depthFirstSearch(n.right);
--- src/com/datastruct/arraytoBST.java
+++ src/com/datastruct/arraytoBST.java
@@ -1,17 +1,17 @@
-package src.com.datastruct;
+package com.datastruct;
 
 public class arraytoBST {
 	public TreeNode converttoBST(int[] arr){
-		return buildBST(arr,0,arr.length);
+		return buildBST(arr,0,arr.length-1);
 	}
 	
 	private TreeNode buildBST(int[] arr, int start, int end){	
-		if (start==end)
+		if(end<start)
 			return null;
 		final int rootIndex=(start+end)/2;
 		final TreeNode n=new TreeNode(arr[rootIndex]);
 		
-		n.left=buildBST(arr,start,rootIndex);
+		n.left=buildBST(arr,start,rootIndex-1);
 		n.right=buildBST(arr,rootIndex+1,end);
 		
 		return n;
