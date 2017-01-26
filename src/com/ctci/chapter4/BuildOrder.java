a4f98ec58e9851d7cd6f311689024ca5bfe09ee1
arunvasudevan <arun.psg.eee@gmail.com>
2017-01-26 08:12:30 -0600

    Ctci Chapter 4- Problem 7

---
+++ src/com/ctci/chapter4/BuildOrder.java
@@ -0,0 +1,16 @@
+package com.ctci.chapter4;
+
+/*
+ * Given a list of projects and a list of dependencies. 
+ * ALl of a projects dependencies must be built before the project is.
+ * Find a build order that will allow the projects to be built.
+ * Input: 
+ * Projects: a, b, c, d, e, f
+ * Dependencies: (d,a), (b,f), (d,b), (a,f), (c,d)
+ * 
+ * Output:  f, e, a, b, d, c
+ */
+
+public class BuildOrder {
+
+}
