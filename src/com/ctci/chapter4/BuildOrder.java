8c33a7ce0f262ade9b13da4217f9ab6222ef0a5e
arunvasudevan <arun.psg.eee@gmail.com>
2017-01-27 08:58:34 -0600

    CtCi Chapter 4 - Problem 7

--- src/com/ctci/chapter4/BuildOrder.java
+++ src/com/ctci/chapter4/BuildOrder.java
@@ -1,5 +1,7 @@
 package com.ctci.chapter4;
 
+import java.util.ArrayList;
+
 /*
  * Given a list of projects and a list of dependencies. 
  * ALl of a projects dependencies must be built before the project is.
@@ -12,5 +14,26 @@ package com.ctci.chapter4;
  */
 
 public class BuildOrder {
+	ArrayList<String> resultList = new ArrayList<String>();
 
+	public ArrayList<String> findBuildOrder(String[] projects,
+			String[][] dependencies) {
+		for (int i = 0; i < projects.length; i++) {
+			String currentProject = projects[i];
+			if (!resultList.contains(currentProject)) {
+				findDependencies(dependencies, currentProject);
+			}
+		}
+		return resultList;
+	}
+
+	private void findDependencies(String[][] dependencies, String currentProject) {
+		for (int i=0; i<dependencies.length; i++) {
+			int j=0;
+			while (currentProject == dependencies[i][j]) {
+				findDependencies(dependencies, currentProject);
+				++j;
+			}
+		}
+	}
 }
