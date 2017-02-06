097cf122c736d45be3e82c951181e8d6eee774dd
Arun Vasudevan <arun.psg.eee@gmail.com>
2017-02-06 10:26:24 -0600

    CtCi CHapter 4 - Problem 8

--- test/com/ctci/chapter4/CommonAncestorTest.java
+++ test/com/ctci/chapter4/CommonAncestorTest.java
@@ -1,45 +1,56 @@
 package com.ctci.chapter4;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
-import com.datastruct.*;
+import com.datastruct.TreeNode;
 
-public class CommonAncestorTest{
-	
+public class CommonAncestorTest {
+
 	TreeNode root, rootL, rootR, rootLL, rootLR, rootRR, rootRL;
 	CommonAncestor ca;
-	
+
 	@Before
-	public void setUp() throws Exception{
+	public void setUp() throws Exception {
 		ca = new CommonAncestor();
-		root = new TreeNode(3);	
-		
+		root = new TreeNode(3);
 		rootL = new TreeNode(1);
-		
 		rootLL = new TreeNode(0);
 		rootLR = new TreeNode(2);
-		
 		rootR = new TreeNode(5);
-		
 		rootRL = new TreeNode(4);
-		rootRR = new TreeNode(6);	
-		
+		rootRR = new TreeNode(6);
+
 		root.left = rootL;
 		root.right = rootR;
-
 		rootL.left = rootLL;
 		rootL.right = rootLR;
-
 		rootR.left = rootRL;
 		rootR.right = rootRR;
 	}
 
 	@Test
-	public void test() {
+	public void testSameLevels() {
+		System.out.println("----------------------------");
+		System.out.println("Same Levels");
+		System.out.println("----------------------------");
 		ca.findCommonAncestor(root, rootRL, rootLR);
 	}
 
+	@Test
+	public void testDifferentLevels() {
+		System.out.println("----------------------------");
+		System.out.println("Different Levels");
+		System.out.println("----------------------------");
+		ca.findCommonAncestor(root, rootR, rootLR);
+	}
+
+	@Test
+	public void testInvalidNode() {
+		System.out.println("----------------------------");
+		System.out.println("Invalid Node");
+		System.out.println("----------------------------");
+		final TreeNode newNode = new TreeNode(9);
+		ca.findCommonAncestor(root, rootL, newNode);
+	}
 }
