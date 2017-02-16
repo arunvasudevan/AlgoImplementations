4663e14b72d1fd59e5012a1f99fdc26deb958ee8
Arun Vasudevan <arun.psg.eee@gmail.com>
2017-02-16 17:43:17 -0600

    Changed Integer to int

--- src/com/ctci/chapter4/CommonAncestor.java
+++ src/com/ctci/chapter4/CommonAncestor.java
@@ -8,53 +8,53 @@ import com.datastruct.TreeNode;
 
 public class CommonAncestor {
 	public TreeNode findCommonAncestor(TreeNode root, TreeNode nodeA, TreeNode nodeB) {
-		final Integer nodeADepth = findDepth(root, nodeA);
-		final Integer nodeBDepth = findDepth(root, nodeB);
+		final int nodeADepth = findDepth(root, nodeA);
+		final int nodeBDepth = findDepth(root, nodeB);
 
 		System.out.println("Node A Depth:" + nodeADepth);
 		System.out.println("Node B Depth:" + nodeBDepth);
 
-		if (nodeADepth == null || nodeBDepth == null) {
+		if (nodeADepth < 0 || nodeBDepth < 0) {
 			return null;
 		}
-		
-		TreeNode higherNode = (nodeADepth > nodeBDepth)? nodeA : nodeB;
+
+		TreeNode higherNode = (nodeADepth > nodeBDepth) ? nodeA : nodeB;
 		final TreeNode lowerNode = (nodeADepth > nodeBDepth) ? nodeB : nodeA;
-		
+
 		higherNode = goUpBy(higherNode, Math.abs(nodeADepth - nodeBDepth));
-		
+
 		return findCommonParent(higherNode, lowerNode);
 	}
 
-	TreeNode findCommonParent(TreeNode nodeA, TreeNode nodeB) {		
-		while(nodeA != nodeB) {
+	TreeNode findCommonParent(TreeNode nodeA, TreeNode nodeB) {
+		while (nodeA != nodeB) {
 			nodeA = nodeA.parent;
 			nodeB = nodeB.parent;
 		}
 		return nodeA;
 	}
-	
+
 	TreeNode goUpBy(TreeNode node, int depthUp) {
-		while(depthUp > 0) {
+		while (depthUp > 0) {
 			depthUp--;
 			node = node.parent;
 		}
 		return node;
 	}
-	
-	Integer findDepth(TreeNode root, TreeNode findNodeDepth) {
+
+	int findDepth(TreeNode root, TreeNode findNodeDepth) {
 		int depth = 0;
 		TreeNode child = null;
-		while(findNodeDepth != null) {
+		while (findNodeDepth != null) {
 			child = findNodeDepth;
 			findNodeDepth = findNodeDepth.parent;
 			++depth;
 		}
-		
-		if(child == root) {
+
+		if (child == root) {
 			return depth;
 		} else {
-			return null;
+			return -1;
 		}
 	}
 }
