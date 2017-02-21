a65cdf4e88b657094b7c8ce276f1cd44118805f3
arunvasudevan <arun.psg.eee@gmail.com>
2017-02-21 07:31:12 -0600

    CtCi Chapter 4 Problem 9

--- src/com/ctci/chapter4/BSTSequence.java
+++ src/com/ctci/chapter4/BSTSequence.java
@@ -14,6 +14,8 @@ import java.util.*;
  */
 
 public class BSTSequence {
+	ArrayList<ArrayList<Integer>> possibleArrays;
+
 	public ArrayList<ArrayList<Integer>> findBSTSequence(TreeNode root) {
 		if (root == null) {
 			return null;
@@ -30,18 +32,20 @@ public class BSTSequence {
 
 		while (!q.isEmpty()) {
 			ArrayList<Integer> arrayByDepth = findArraybyDepth(q);
-			
-			if(arrayByDepth.size() == 0) {
+
+			if (arrayByDepth.size() == 0) {
 				break;
 			}
-			
-			ArrayList<ArrayList<Integer>> possibleArrays = generateCombinations(arrayByDepth);
-			
-			ArrayList<ArrayList<Integer>> copyResult = result;			
+
+			// possibleArrays = generateCombinations(arrayByDepth);
+			possibleArrays = new ArrayList<ArrayList<Integer>>();
+			arrayPermutations(arrayByDepth, 0);
+
+			ArrayList<ArrayList<Integer>> copyResult = result;
 			result = new ArrayList<ArrayList<Integer>>();
-			
-			for(ArrayList<Integer> resultArray: copyResult) {
-				for(ArrayList<Integer> possibleArrayList: possibleArrays) {
+
+			for (ArrayList<Integer> resultArray : copyResult) {
+				for (ArrayList<Integer> possibleArrayList : possibleArrays) {
 					ArrayList<Integer> interimResult = new ArrayList<Integer>();
 					interimResult.addAll(resultArray);
 					interimResult.addAll(possibleArrayList);
@@ -52,38 +56,55 @@ public class BSTSequence {
 
 		return result;
 	}
-	
-	private ArrayList<ArrayList<Integer>> generateCombinations(ArrayList<Integer> arrayByDepth){
+
+	private ArrayList<ArrayList<Integer>> generateCombinations(
+			ArrayList<Integer> arrayByDepth) {
 		ArrayList<ArrayList<Integer>> possibleArrays = new ArrayList<ArrayList<Integer>>();
 		possibleArrays.add(arrayByDepth);
-		
+
 		ArrayList<Integer> combinationArray = new ArrayList<Integer>();
-		for(int i=arrayByDepth.size()-1; i>=0; i--) {
+		for (int i = arrayByDepth.size() - 1; i >= 0; i--) {
 			combinationArray.add(arrayByDepth.get(i));
 		}
-		
+
 		possibleArrays.add(combinationArray);
 		return possibleArrays;
 	}
 
-	private ArrayList<Integer> findArraybyDepth(
-			Queue<TreeNode> q) {
+	private void arrayPermutations(ArrayList<Integer> arrayByDepth, int index) {
+		if (index >= arrayByDepth.size() - 1) {
+
+			ArrayList<Integer> copyPossibleArray = new ArrayList<Integer>();
+			for (Integer a : arrayByDepth) {
+				copyPossibleArray.add(a);
+			}
+			possibleArrays.add(copyPossibleArray);
+			return;
+		}
+		for (int i = index; i < arrayByDepth.size(); i++) {
+			Collections.swap(arrayByDepth, i, index);
+			arrayPermutations(arrayByDepth, index + 1);
+			Collections.swap(arrayByDepth, index, i);
+		}
+	}
+
+	private ArrayList<Integer> findArraybyDepth(Queue<TreeNode> q) {
 		ArrayList<Integer> depthArray = new ArrayList<Integer>();
 		Queue<TreeNode> childQ = new LinkedList<TreeNode>();
-		
-		while(!q.isEmpty()) {
+
+		while (!q.isEmpty()) {
 			TreeNode node = q.remove();
 			if (node.left != null) {
 				depthArray.add(node.left.data);
 				childQ.add(node.left);
-			} 
+			}
 			if (node.right != null) {
 				depthArray.add(node.right.data);
 				childQ.add(node.right);
 			}
-		}	
+		}
 		q.addAll(childQ);
-		
+
 		return depthArray;
 	}
 }
